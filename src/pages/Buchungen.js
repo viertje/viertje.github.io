3a4a2e410bade19e3887856959afb62896e1e3e6
vier <oliviervroege@hotmail.com>
2024-07-11 09:40:50 +0200

    last minor tweaks

--- src/pages/Buchungen.js
+++ src/pages/Buchungen.js
@@ -32,13 +32,7 @@ export default function Buchungen() {
                 <div className='flex justify-center mb-8'>
                 <div className='text-4xl '>Buchungen</div>
             </div>
-        <div className="bg-gradient-to-r from-blue-900 to-neutral-800 rounded-md gap-8 p-2 flex justify-between">
-            <div className="flex gap-2 mx-8 place-items-center">
-            <div className='text-center'>Name:</div>
-            <input className="text-black rounded-md text-center hover:scale-105 m-4 h-full w-72 bg-white" />
-            </div> 
-            <button className="hover:scale-105 text-black py-2 px-8 bg-gradient-to-r from-gray-200 to-indigo-900 rounded-md">Suchen</button>
-        </div>
+
         <div className="bg-gradient-to-r from-blue-900 to-neutral-800 rounded-md gap-8 p-2 flex flex-col justify-between mt-8 h-auto">
             <div className='place-self-center text-4xl'>Flüge</div>
             {flights.map((i, key) => {
